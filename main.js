5cf40d0197525dd28a65d0c83414aa9466a19ada
Anburaj5253 <anburajk112000@gmail.com>
2023-08-23 10:55:59 +0530

    add

---
+++ main.js
@@ -0,0 +1,23 @@
+ // let sum=[1,2,3,5,6,8];
+ // for(i=0;i<numarray.length;i++)
+	// {
+	// document.write(numarray [i]);
+	// document.write("<br>");
+	
+// }
+
+// let num=[[1,3,'four'],[5,6,'seven']];
+  // for(i=0;i<num.length;i++)
+     // {
+	// for(j=0;j<num[i].length;j++)
+	  // {
+		  
+	// console.log(num [i][j]);
+	
+	 // } 
+	 // } 
+	 
+let name=["html","css","javascript"];
+console.log(name);
+name.push("react");
+console.log(name);
